483576bf8a7b03bb97523458439ec536fe48507f
Michael Head <michaelehead@gmail.com>
2017-10-12 17:15:43 -0400

    CSS in component now. Overlay added. Destroy method works

--- index.js
+++ index.js
@@ -6,18 +6,21 @@ const path = require('path');
 app.use(express.static('./dist'));
 
 app.get('/', function (req, res) {
-  const bookmarklet = fs.readFileSync('./dist/bookmarklet.js');
+  const bookmarklet = fs.readFileSync('./dist/PriceTrackerBookmarklet.js');
 
   res.send(`
     <!doctype html>
     <html lang="en">
     <head>
       <title>Bookmarklet Test Page</title>
-      <link rel="stylesheet" href="bookmarklet.css" />
     </head>
     <body>
-      <main></main>
-      <a href="${bookmarklet}">Open Bookmarklet</a>
+      <p>
+        The bookmarklet runs automatically on page load as an included script tag at the end of the document, since
+        it's an immediately-invoked function expression (IIFE). You can also click <a href="${bookmarklet}">Open Bookmarklet</a> 
+        to invoke the bookmarklet if you close it. 
+      </p>
+      <div>A sample price: <span class="price">$99.99</span></div>
       <script src="PriceTracker.js"></script>
     </body>  
     </html>
